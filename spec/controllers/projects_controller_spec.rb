c17a57cfd934f494d8d15d67981d4490b545f528
Anthony Hernandez <ahernandez@marqeta.com>
2017-04-04 15:58:45 -0700

    Add logic (+test) to ProjectController to find projects or raise 404-type alert

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
 
+  it 'handles a missing project correctly' do
+    error_message = 'The project you were looking for could not be found.'
+    get :show, id: 'fake-id'
+
+    expect(response).to redirect_to(projects_path)
+
+    expect(flash[:alert]).to eq error_message
+  end
 end
